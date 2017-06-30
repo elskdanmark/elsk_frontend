4eeba5a7a6e6a91f13c1d62134d7e37ef3c13a30
gislainh <hardsongis@gmail.com>
2017-06-30 04:01:32 +0200

    Add loggedIn Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,28 +15,38 @@ import { NavigationComponent } from './themes/navigation/navigation.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailComponent } from './users/user-detail.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import {AUTH_PROVIDERS} from './services/auth-service' ;
+import {LoggedInGuard} from './guards/loggedIn.guard' ;
 
 const appRoutes: Routes = [
   { 
     path: 'users', component:UsersComponent,
-     data: { title: 'All Users'}
+    data: { title: 'All Users',
+    canActivate: [LoggedInGuard],
+  }
   },
-  { path: 'users/:id',      component: UserDetailComponent },
+  { path: 'users/:id', component: UserDetailComponent ,
+    canActivate: [LoggedInGuard]},
   {
     path: 'recipients',
     component: UsersComponent,
-    data: { title: 'Recipients', group:'recipient'}
+    data: { title: 'Recipients', group:'recipient'},
+    canActivate: [LoggedInGuard],
   },
     {
     path: 'volunteers',
     component: UsersComponent,
-    data: { title: 'Volunteers', group:'volunteer'}
+    data: { title: 'Volunteers', group:'volunteer'},
+    canActivate: [LoggedInGuard],
   },
   { path: '',
     redirectTo: '/users',
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [LoggedInGuard],
   },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent,
+    canActivate: [LoggedInGuard],
+  }
 ];
 
 @NgModule({
@@ -58,7 +68,12 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [UserService, WsseService],
+  providers: [
+    UserService, 
+    WsseService,
+    AUTH_PROVIDERS,
+    LoggedInGuard,
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
